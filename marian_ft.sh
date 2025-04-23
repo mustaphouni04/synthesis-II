bc4da7608357c6ba8d19aa44cc8aa4d9c36dfa2b
mustaphouni04 <elaichounimustapha@gmail.com>
2025-04-23 16:24:08 +0200

    add bash script for finetuning

---
+++ marian_ft.sh
@@ -0,0 +1,24 @@
+TRAIN_SET=../Files/train_set.csv
+VALID_SET=../Files/valid_set.csv
+TEST_SET=../Files/test_set.csv
+OUTPUT_DIR=/tmp/tst_translation
+TRAIN_BATCH_SIZE=4
+TEST_BATCH_SIZE=4
+MAX_SOURCE_LENGTH=512
+
+
+python run_translation.py \
+--model_name_or_path Helsinki-NLP/opus-mt-en-es \
+--do_train \
+--do_eval \
+--source_lang en \
+--target_lang es \
+--train_file $TRAIN_SET \
+--validation_file $VALID_SET \
+--output_dir $OUTPUT_DIR \
+--per_device_train_batch_size=$TRAIN_BATCH_SIZE \
+--per_device_eval_batch_size=$TEST_BATCH_SIZE \
+--overwrite_output_dir \
+--test_file $TEST_SET \
+--predict_with_generate \
+--max_source_length=$MAX_SOURCE_LENGTH
